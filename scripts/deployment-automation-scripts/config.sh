1f13d0861522d20e1b2a8176967fdf688c1a5acd
Qays Alnajjad <103290698+QaysAlnajjad@users.noreply.github.com>
2026-03-07 01:23:15 +0400

    Update and rename config.yaml to config.sh

---
+++ scripts/deployment-automation-scripts/config.sh
@@ -0,0 +1,42 @@
+############################################
+#  AWS Regions
+############################################
+PRIMARY_REGION="us-east-1"
+DR_REGION="ca-central-1"
+
+############################################
+#  Terraform Backend Config
+############################################
+TF_STATE_BUCKET_NAME="terraform-state-1011202555"
+TF_STATE_BUCKET_REGION="eu-central-1"
+
+############################################
+#  Docker / Container Config
+############################################
+DOCKERHUB_IMAGE="qaysalnajjad/ecs-wordpress-app:v3.6"
+ECR_REPO_NAME="ecs-wordpress-app"
+
+############################################
+#  Media S3 buckets
+############################################
+PRIMARY_MEDIA_S3_BUCKET="wordpress-media-primary-2004"
+DR_MEDIA_S3_BUCKET="wordpress-media-dr-2004"
+
+############################################
+#  Media S3 buckets
+############################################
+RDS_IDENTIFIER="wordpress-rds"
+
+############################################
+#  Domain and hosted zone
+############################################
+HOSTED_ZONE_ID=""
+PRIMARY_DOMAIN="example.com"   # Primary custom domain without www (e.g., yourdomain.com)
+CERTIFICATE_SANs='["*.example.com"]'
+
+############################################
+#  SSL certificates
+############################################
+PRIMARY_ALB_SSL_CERTIFICATE_ARN=""
+DR_ALB_SSL_CERTIFICATE_ARN=""
+CLOUDFRONT_SSL_CERTIFICATE_ARN=""
