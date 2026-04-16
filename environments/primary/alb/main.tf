3023080c5dc4ed0fb14c51316199e67ede278058
qays-test <qaysalnajjad317@gmail.com>
2026-04-16 18:32:08 +0400

    Update main.tf

--- environments/primary/alb/main.tf
+++ environments/primary/alb/main.tf
@@ -6,7 +6,7 @@ data "terraform_remote_state" "network" {
   backend = "s3"
   config = {
     bucket = var.state_bucket_name
-    key = "environments/primary/network_rds/terraform.tfstate"
+    key = "environments/primary/network/terraform.tfstate"
     region = var.state_bucket_region
   }  
 }
