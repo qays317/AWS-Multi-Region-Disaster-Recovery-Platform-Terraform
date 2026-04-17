82f9410b4b777e65cf983a6c9d6caf1c6d75d54b
qays-test <qaysalnajjad317@gmail.com>
2026-04-17 12:23:23 +0400

    Update outputs.tf

--- modules/lambda/outputs.tf
+++ modules/lambda/outputs.tf
@@ -6,3 +6,19 @@ output "snf_functions_arns" {
     value = try({for k, v in aws_lambda_function.main : k => v.arn
                   if lookup( v.tags, "Component", "") == "DR orchestration"}, null)   
 }
+
+output "check_replica_readiness_arn" {
+    value = try({for k, v in aws_lambda_function.main : k => v.arn
+                  if lookup( v.tags, "Name", "") == "check-replica-readiness"}, null)   
+}
+
+output "promote_replica_arn" {
+    value = try({for k, v in aws_lambda_function.main : k => v.arn
+                  if lookup( v.tags, "Name", "") == "promote-replica"}, null)   
+}
+
+
+output "check_db_available_arn" {
+    value = try({for k, v in aws_lambda_function.main : k => v.arn
+                  if lookup( v.tags, "Name", "") == "check-db-available"}, null)   
+}
