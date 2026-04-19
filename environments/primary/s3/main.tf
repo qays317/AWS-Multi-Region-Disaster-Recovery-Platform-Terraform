800c27ff15dfcb3cb707fc7ff52841a7dccfd6e2
qays-test <qaysalnajjad317@gmail.com>
2026-04-19 23:25:38 +0400

    Update main.tf

--- environments/primary/s3/main.tf
+++ environments/primary/s3/main.tf
@@ -2,6 +2,16 @@
 //    S3
 //==================================================================================
 
+
+data "terraform_remote_state" "network" {
+  backend = "s3"
+  config = {
+    bucket = var.state_bucket_name
+    key = "environments/primary/network/terraform.tfstate"
+    region = var.state_bucket_region
+  }
+}
+
 module "s3" {
     source = "../../../modules/s3"
     
@@ -9,5 +19,5 @@ module "s3" {
 
     cloudfront_distribution_arn = var.cloudfront_distribution_arn
     ecs_task_role_arn = var.ecs_task_role_arn
-    s3_vpc_endpoint_id = var.s3_vpc_endpoint_id
+    s3_vpc_endpoint_id = data.terraform_remote_state.outputs.s3_vpc_endpoint_id
 }
