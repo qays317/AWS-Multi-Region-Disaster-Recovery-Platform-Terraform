20695cb54ef75771b1ed068ff91cc08c82a31f5b
qays-test <qaysalnajjad317@gmail.com>
2026-04-19 20:14:02 +0400

    Update locals.tf

--- environments/primary/rds/locals.tf
+++ environments/primary/rds/locals.tf
@@ -28,7 +28,7 @@ locals {
         layer = true
         environment = {
             MASTER_SECRET_ARN   = module.rds.master_secret_arn
-            WORDPRESS_SECRET_NAME = "${var.rds_identifier}-secret"
+            WORDPRESS_SECRET_NAME = module.rds.wordpress_secret_arn
             DB_HOST = split(":", module.rds.rds_endpoint)[0]
             DB_PORT = tostring(module.rds.rds_port)
             WORDPRESS_DB_NAME  = local.rds_config.db_name
