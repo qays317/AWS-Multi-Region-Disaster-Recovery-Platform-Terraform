720745ed8e608e9274c28773f28804d10452cc58
qays-test <qaysalnajjad317@gmail.com>
2026-04-22 18:52:29 +0400

    Update outputs.tf

--- modules/lambda/outputs.tf
+++ modules/lambda/outputs.tf
@@ -2,30 +2,18 @@ output "primary_db_setup_name" {
   value = try(aws_lambda_function.main["primary-db-setup"].function_name, null)
 }
 
-output "check_replica_readiness_arn" {
-  value = try(aws_lambda_function.main["check-replica-readiness"].arn, null)
+output "replica_failover_handler_arn" {
+  value = aws_lambda_function.main["replica-failover-handler"].arn
 }
 
-output "promote_replica_arn" {
-  value = try(aws_lambda_function.main["promote-replica"].arn, null)
-}
-
-output "check_db_available_arn" {
-  value = try(aws_lambda_function.main["check-db-available"].arn, null)
+output "service_recovery_handler_arn" {
+  value = aws_lambda_function.main["service-recovery-handler"].arn
 }
 
 output "validate_db_writable_arn" {
-  value = try(aws_lambda_function.main["validate-db-writable"].arn, null)
-}
-
-output "scaleup_dr_service_arn" {
-  value = try(aws_lambda_function.main["scaleup-dr-service"].arn, null)
-}
-
-output "check_ecs_healthy_arn" {
-  value = try(aws_lambda_function.main["check-ecs-healthy"].arn, null)
+  value = aws_lambda_function.main["validate-db-writable"].arn
 }
 
 output "validate_application_arn" {
-  value = try(aws_lambda_function.main["validate-application"].arn, null)
+  value = aws_lambda_function.main["validate-application"].arn
 }
