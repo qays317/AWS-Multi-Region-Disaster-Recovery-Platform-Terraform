a070f24ae591682d532bfcdf07287ae1faea75ca
qays-test <qaysalnajjad317@gmail.com>
2026-04-22 19:41:36 +0400

    Update outputs.tf

--- modules/lambda/outputs.tf
+++ modules/lambda/outputs.tf
@@ -3,17 +3,17 @@ output "primary_db_setup_name" {
 }
 
 output "replica_failover_handler_arn" {
-  value = aws_lambda_function.main["replica-failover-handler"].arn
+  value = try(aws_lambda_function.main["replica-failover-handler"].arn, null)
 }
 
 output "service_recovery_handler_arn" {
-  value = aws_lambda_function.main["service-recovery-handler"].arn
+  value = try(aws_lambda_function.main["service-recovery-handler"].arn, null)
 }
 
 output "validate_db_writable_arn" {
-  value = aws_lambda_function.main["validate-db-writable"].arn
+  value = try(aws_lambda_function.main["validate-db-writable"].arn, null)
 }
 
 output "validate_application_arn" {
-  value = aws_lambda_function.main["validate-application"].arn
+  value = try(aws_lambda_function.main["validate-application"].arn, null)
 }
