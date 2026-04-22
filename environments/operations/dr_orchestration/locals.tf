1cc116b799c34af19def5cb2f37db6ffca5fb3fc
qays-test <qaysalnajjad317@gmail.com>
2026-04-22 16:21:23 +0400

    Update locals.tf

--- environments/operations/dr_orchestration/locals.tf
+++ environments/operations/dr_orchestration/locals.tf
@@ -1,96 +1,40 @@
-
 locals {
-  lambda = {
-      
-      "check-replica-readiness" ={ 
-        timeout = 60
-        role_arn = data.terraform_remote_state.iam.outputs.lambda_failover_role_arn
-        layer = false
-        environment = {
-          DR_REGION                   = var.dr_region
-          DR_REPLICA_IDENTIFIER       = var.rds_replica_identifier
-          MAX_REPLICATION_LAG_SECONDS = tostring(var.max_replication_lag_seconds)
-        }
-        component = "DR Orchestration"
+  checks = {
+    "replica-failover-handler" = {
+      timeout = 300
+      environment = {
+        DR_REGION                   = var.dr_region
+        DR_REPLICA_IDENTIFIER       = var.rds_replica_identifier
+        MAX_REPLICATION_LAG_SECONDS = tostring(var.max_replication_lag_seconds)
       }
+    }
 
-      "promote-replica" = {
-        timeout = 60
-        role_arn = data.terraform_remote_state.iam.outputs.lambda_failover_role_arn
-        layer = false
-        environment = {
-          DR_REGION             = var.dr_region
-          DR_REPLICA_IDENTIFIER = var.rds_replica_identifier
-        }
-        component = "DR Orchestration"
+    "service-recovery-handler" = {
+      timeout = 300
+      environment = {
+        DR_REGION        = var.dr_region
+        ECS_CLUSTER_NAME = var.ecs.cluster_name
+        ECS_SERVICE_NAME = var.ecs.service_name
+        DR_DESIRED_COUNT = tostring(var.ecs.desired_count)
       }
-      
-      "check-db-available" = {
-        timeout = 60
-        role_arn = data.terraform_remote_state.iam.outputs.lambda_failover_role_arn
-        layer = false
-        environment = {
-          DR_REGION             = var.dr_region
-          DR_REPLICA_IDENTIFIER = var.rds_replica_identifier
-        }
-        component = "DR Orchestration"
-      }
+    }
 
-      "validate-db-writable" = {
-        timeout = 60
-        role_arn = data.terraform_remote_state.iam.outputs.lambda_failover_role_arn
-        layer = true
-        vpc_config = {
-          subnet_ids = data.terraform_remote_state.network.outputs.private_subnets_ids
-          security_group_ids = [data.terraform_remote_state.network.outputs.lambda_validate_db_writable_sg_id]
-        }
-        environment = {
-          DB_SECRET_ARN = data.terraform_remote_state.dr_rds.outputs.wordpress_secret_arn
-          DB_CONNECT_TIMEOUT = tostring(var.db_connect_timeout)
-          DR_REGION = var.dr_region
-        }
-        component = "DR Orchestration"
+    "validate-db-writable" = {
+      timeout = 60
+      environment = {
+        DB_SECRET_ARN      = data.terraform_remote_state.dr_rds.outputs.wordpress_secret_arn
+        DB_CONNECT_TIMEOUT = tostring(var.db_connect_timeout)
+        DR_REGION          = var.dr_region
       }
+    }
 
-      "scaleup-dr-service" = {
-        timeout = 60
-        role_arn = data.terraform_remote_state.iam.outputs.lambda_failover_role_arn
-        layer = false
-        environment = {
-          DR_REGION        = var.dr_region
-          ECS_CLUSTER_NAME = var.ecs_cluster_name
-          ECS_SERVICE_NAME = var.ecs_service_name
-          DR_DESIRED_COUNT = tostring(var.dr_desired_count)
-        }
-        component = "DR Orchestration"
+    "validate-application" = {
+      timeout = 30
+      environment = {
+        APP_HEALTHCHECK_URL     = var.app_healthcheck.path
+        APP_HEALTHCHECK_TIMEOUT = tostring(var.app_healthcheck.timeout)
+        EXPECTED_STATUS_CODE    = tostring(var.app_healthcheck.status_code)
       }
-    
-      "check-ecs-healthy" = {
-        timeout = 60
-        role_arn = data.terraform_remote_state.iam.outputs.lambda_failover_role_arn
-        layer = false
-        environment = {
-          DR_REGION        = var.dr_region
-          ECS_CLUSTER_NAME = var.ecs_cluster_name
-          ECS_SERVICE_NAME = var.ecs_service_name
-        }
-        component = "DR Orchestration"
-      }
-    
-      "validate-application" = {
-        timeout = 30
-        role_arn = data.terraform_remote_state.iam.outputs.lambda_failover_role_arn
-        layer = false
-        environment = {
-          APP_HEALTHCHECK_URL     = var.app_healthcheck.path
-          APP_HEALTHCHECK_TIMEOUT = tostring(var.app_healthcheck.timeout)
-          EXPECTED_STATUS_CODE    = tostring(var.app_healthcheck.status_code)
-        }
-        component = "DR Orchestration"
-      }
+    }
   }
-}
-
-locals {
-  lambda_source_base = "${path.module}/../../../lambdas"
 }
